315425497ff3428b858b9b7f596ba56fbe605513
Milind <milind.singh@tcs.com>
2017-09-03 19:07:06 +0000

    valid and invalid response handling

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.load();
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
-////////////////////////////////////////////////////////////////////////////
+//////////////////////TODO - INCLUDE JS USING REQUIRE//////////////////////////////////////////////////////
 
 var requirejs = require('requirejs');
 requirejs.config({
@@ -97,7 +97,7 @@ const wwoApi = apiai(process.env.WWO_API_KEY);
 const wwoApiKey = process.env.WWO_API_KEY;
 
 var createCurrentWeatherHTML = function(currConditions, locationString) {
-    var html = '<div class="col-xs-6">';
+    var html = '<div class="col-xs-6 text-center">';
     html += "<h3>" + locationString + "</h3>"; // query string. In this case Liverpool, United Kingdom
     html += "<p>Current: <strong>" + currConditions[0].weatherDesc[0].value + "</strong></p>"; // get the current weather description
     html += "<p>Wind:" + currConditions[0].winddir16Point + " at " + currConditions[0].windspeedMiles + " mph/" + currConditions[0].windspeedKmph + " m/h</p>"; // wind details
@@ -107,14 +107,19 @@ var createCurrentWeatherHTML = function(currConditions, locationString) {
     return html;
 };
 
+
+function dayOfWeekAsString(dayIndex) {
+    return ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"][dayIndex];
+}
+
 var createDaySummaryHTML = function(daysWeather) {
     //var imgSrc = "/icons/" + daysWeather.hourly[0].weatherCode + '.png';
     var imgSrc = daysWeather.hourly[0].weatherIconUrl[0].value;
-    var html = '<div class="col-xs-6">';
+    var html = '<div class="col-xs-6 text-center">';
     html += '<div class="day">' + dayOfWeekAsString(new Date(daysWeather.date).getDay())+ '</div>';
     html += '<div class="icon"><img src="' + imgSrc + '" alt="" /></div>';
-    html += '<div class="celsius">' + daysWeather.maxtempC + 'Â°C | ' + daysWeather.mintempC + 'Â°C</div>';
-    html += '<div class="farenheit">' + daysWeather.maxtempF + 'Â°F | ' + daysWeather.mintempF + 'Â°F</div>';
+    html += '<div class="celsius">' + daysWeather.maxtempC + ' °C | ' + daysWeather.mintempC + ' °C</div>';
+    html += '<div class="farenheit">' + daysWeather.maxtempF + ' °F | ' + daysWeather.mintempF + ' °F</div>';
     html += "</div>";
 
     return html;
@@ -122,9 +127,6 @@ var createDaySummaryHTML = function(daysWeather) {
 
 
 
-function dayOfWeekAsString(dayIndex) {
-    return ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"][dayIndex];
-}
 
 app.post('/wwo', function (req, res) {
   
@@ -133,30 +135,36 @@ app.post('/wwo', function (req, res) {
       var date = d.toLocaleDateString();
       var requestUrl = 'https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json&num_of_days=1' +
     '&q=' + encodeURIComponent(city) + '&key='+wwoApiKey+'&date=' + date;
-  
+      var html = "";
   
     request(requestUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-
+        if (!error && response.statusCode == 200 ) {
+            
             // parse the json result
             var result = JSON.parse(body);
-            var html = "";//'<table style="font-size: 10px; font-family: Arial, Helvetica, sans-serif">';
-            var currentWeather = result.data.current_condition;
-            var weeklyWeather = result.data.weather;
-
-            html += '<div class="weather-widget col-xs-12 well">';
-
-            html += createCurrentWeatherHTML(currentWeather, result.data.request[0].query);
-
-            weeklyWeather.forEach(function(day) {
-                html += createDaySummaryHTML(day);
-            });
-
-            html += '</div>';          
+            //check for valid and invalid input response 
+            if(result.data.request != undefined && result.data.request[0].query.length > 0){
+                //'<table style="font-size: 10px; font-family: Arial, Helvetica, sans-serif">';
+                var currentWeather = result.data.current_condition;
+                var weeklyWeather = result.data.weather;
+    
+                html += '<div class="weather-widget col-xs-12 well">';
+    
+                html += createCurrentWeatherHTML(currentWeather, result.data.request[0].query);
+    
+                weeklyWeather.forEach(function(day) {
+                    html += createDaySummaryHTML(day);
+                });
+    
+                html += '</div>';  
+            }else{
+              html += result.data.error[0].msg;
+            }    
 
           res.send(html);
         } else {
            console.log(error, response.statusCode, body);
+           html += "Your request could not be completed due to an internal error, please try after some time";
         }
         res.end("");
     });
